5243721243c8cd5e45641fe08e7f5f8095be4f05
Dorina Kamp <dorina.kamp@gmail.com>
2022-11-08 20:11:24 +0100

    london2

--- src/App2.js
+++ src/App2.js
@@ -19,25 +19,7 @@ const App2 = () => {
       <button
         onClick={() => {
           //changing components depending on interaction
-          setResponse(<Header city={"Utrecht"}></Header>);
-
-          axios
-            //axios-method
-            //get functions gets one parameter the URL with all its differnt parts: Domain, Path, Key and Values
-            .get(
-              //"https://api.weatherapi.com/v1/search.json?key=55002895a5484b73bae191557220711&q=Amstelveen"
-              "http://api.weatherapi.com/v1/sports.json?key=55002895a5484b73bae191557220711&q=London"
-            )
-            .then((response) => {
-              console.log(response.data);
-              const footballComps = response.data.football.map((match) => {
-                return <div>{match.match}</div>;
-              });
-              setResponse(footballComps);
-            })
-            .catch((e) => {
-              console.log("Error: " + e);
-            });
+          setResponse(<FootballMatches city={"Utrecht"}></FootballMatches>);
         }}
       >
         Utrecht!
@@ -58,7 +40,7 @@ const FootballMatches = (props) => {
       //get functions gets one parameter the URL with all its differnt parts: Domain, Path, Key and Values
       .get(
         //"https://api.weatherapi.com/v1/search.json?key=55002895a5484b73bae191557220711&q=Amstelveen"
-        "http://api.weatherapi.com/v1/sports.json?key=55002895a5484b73bae191557220711&q=London"
+        "http://api.weatherapi.com/v1/sports.json?key=55002895a5484b73bae191557220711&q="+props.city
       )
       .then((response) => {
         console.log(response.data);
