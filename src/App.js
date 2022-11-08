8bf45847ce0557a8474aa3133228c643df71f66e
Dorina Kamp <dorina.kamp@gmail.com>
2022-11-08 19:02:01 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import axios from "axios";
 
 function App() {
   axios
+  //axios-method 
+  //get functions gets one parameter the URL with all its differnt parts: Domain, Path, Key and Values
     .get(
-      "https://api.weatherapi.com/v1/search.json?key=55002895a5484b73bae191557220711&q=Utrecht"
+      //"https://api.weatherapi.com/v1/search.json?key=55002895a5484b73bae191557220711&q=Amstelveen"
+      'http://api.weatherapi.com/v1/sports.json?key=55002895a5484b73bae191557220711&q=Amsterdam'
     )
     .then((response) => {
       console.log(response.data);
