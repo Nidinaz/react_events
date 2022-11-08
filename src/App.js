e8a83c17d376c574feb91f42290407f189974f44
Mohammad Al Kalaleeb <mohammadautumn@outlook.com>
2022-11-08 18:37:40 +0100

    added axios and performed a request

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  axios
+    .get(
+      "https://api.weatherapi.com/v1/search.json?key=55002895a5484b73bae191557220711&q=Utrecht"
+    )
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((e) => {
+      console.log("Error: " + e);
+    });
+
+  return <div></div>;
 }
 
 export default App;
